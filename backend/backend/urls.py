e3086951bf3c4e4c0f2eb1d9920c5f6bae97ac0d
Sonu Pal <sonupal0840@example.com>
2025-07-17 22:55:59 +0530

    get code from github

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('userapi/', include('advocateshub.urls')),  # so frontend hits /userapi/register/
-    path('api/', include('website_feedback.urls')), 
+    path('', include('website_feedback.urls')), 
+    path('api/', include('reviews.urls')), 
 ]
 
 
