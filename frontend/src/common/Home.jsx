571cf7b043b99d38a3af060483e1067e0247c535
sonu pal <sonupal0840@gmail.com>
2025-07-30 00:04:09 +0530

    revert Isha Changes and add website feedback page

--- frontend/src/common/Home.jsx
+++ frontend/src/common/Home.jsx
@@ -6,6 +6,7 @@ import Practice from './homepage/Practice'
 import Featured from './homepage/Featured'
 import Testimonials from './homepage/Testimonials'
 import ContactUs from './homepage/ContactUs'
+import WebsiteFeedbackSection from './homepage/WebsiteFeedbackSection'
 
 
 const Home = () => {
@@ -17,6 +18,7 @@ const Home = () => {
         <Practice />
         <Featured />
         <Testimonials />
+        <WebsiteFeedbackSection/>
         <ContactUs />
     </>
   )
